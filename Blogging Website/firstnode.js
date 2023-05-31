5bf58162536580775abfea722ad9cc50570f42e2
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-06-01 00:09:46 +0530

    Add files via upload

---
+++ Blogging Website/firstnode.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! This is Sky. How are you?');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
